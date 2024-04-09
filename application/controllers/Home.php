496d394b6ceb35c582f02fe6aba818d63cad06ea
Ribeirovisk <franckrm2001@gmail.com>
2024-04-09 09:52:12 -0300

    feat: Separete navbar, sidebar, header and footer into layout files

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,6 +5,8 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
+		$this->load->view('layout/header');
 		$this->load->view('home/index');
+		$this->load->view('layout/footer');
 	}
 }
