9fb2ee10ddd4fd2e1148d07a18e069e319e89827
Ribeirovisk <franckrm2001@gmail.com>
2024-04-10 20:39:50 -0300

    feat: set option config dynamically title via controller

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -5,15 +5,21 @@
                 <?php $this->load->view('layout/sidebar') ?>
                 <div class="main-content">
                     <div class="container-fluid">
-                       <h1>Home</h1>
+    
+					
+
+
+
+
+
                     </div>
                 </div>
 
 
                 <footer class="footer">
                     <div class="w-100 clearfix">
-                        <span class="text-center text-sm-left d-md-inline-block">Copyright © 2018 ThemeKit v2.0. All Rights Reserved.</span>
-                        <span class="float-none float-sm-right mt-1 mt-sm-0 text-center">Crafted with <i class="fa fa-heart text-danger"></i> by <a href="http://lavalite.org/" class="text-dark" target="_blank">Lavalite</a></span>
+                        <span class="text-center text-sm-left d-md-inline-block">Copyright © <?php echo date('Y')?> ThemeKit v2.0. All Rights Reserved.</span>
+                        <span class="float-none float-sm-right mt-1 mt-sm-0 text-center">Crafted with <i class="fa fa-code text-dark"></i> by <a href="javascript:void" class="text-dark" target="_blank">Francisco Miguel</a></span>
                     </div>
                 </footer>
                 
