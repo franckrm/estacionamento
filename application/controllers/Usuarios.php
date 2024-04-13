4c96e91aa73f0e4b66eacf740ca39bc60ea071b8
Ribeirovisk <franckrm2001@gmail.com>
2024-04-13 10:27:43 -0300

    feat: create update page for user

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -1,24 +1,26 @@
-<?php 
+<?php
 
 defined('BASEPATH') or exit('Ação  não permitida');
 
-class Usuarios extends CI_Controller{
+class Usuarios extends CI_Controller
+{
 
-	public function index(){
+	public function index()
+	{
 
 		$data = array(
 			'titulo' => 'Usuarios cadastrados',
 			'sub_titulo' => 'Chegou a hora de listar os usuários cadastrados no banco de dados',
-			'styles'=> array(
+			'styles' => array(
 				'plugins/datatables.net-bs4/css/dataTables.bootstrap4.min.css'
 			),
-			'scripts' =>array(
+			'scripts' => array(
 				'plugins/datatables.net/js/jquery.dataTables.min.js',
 				'plugins/datatables.net-bs4/js/dataTables.bootstrap4.min.js',
 				'plugins/datatables.net/js/estacionamento.js',
 			),
-			'usuarios'=> $this->ion_auth->users()->result(),
-			
+			'usuarios' => $this->ion_auth->users()->result(),
+
 		);
 		// echo '<pre>';
 		// print_r($this);
@@ -30,30 +32,27 @@ class Usuarios extends CI_Controller{
 	}
 
 
-	public function core($usuario_id = NULL){
+	public function core($usuario_id = NULL)
+	{
 
-		if(!$usuario_id){
+		if (!$usuario_id) {
 			exit('Pode cadastrar novo usuário');
-		}else{
-			if(!$this->ion_auth->user($usuario_id)->row()){
+		} else {
+			if (!$this->ion_auth->user($usuario_id)->row()) {
 				exit('Usuário não existe');
-			}else{
+			} else {
 				//Editar usuário
 				$data = array(
 					'titulo' => 'Editar Usuário',
 					'sub_titulo' => 'Chegou a hora de editar o usuário',
-					'usuario'=> $this->ion_auth->user($usuario_id)->row(),
-					'icone_view'=> 'ik ik-user'
-					
+					'usuario' => $this->ion_auth->user($usuario_id)->row(),
+					'icone_view' => 'ik ik-user'
+
 				);
 				$this->load->view('layout/header', $data);
 				$this->load->view('usuarios/core');
 				$this->load->view('layout/footer');
 			}
-
 		}
-
-		
 	}
-
 }
--- application/views/usuarios/core.php
+++ application/views/usuarios/core.php
@@ -1,85 +1,114 @@
-
-<?php $this->load->view('layout/navbar');?>
+<?php $this->load->view('layout/navbar'); ?>
 
 <div class="page-wrap">
-<?php $this->load->view('layout/sidebar') ?>
-<div class="main-content">
-	<div class="container-fluid">
-		<div class="page-header">
-			<div class="row align-items-end">
-				<div class="col-lg-8">
-					<div class="page-header-title">
-						<i class="<?php echo $icone_view;?> bg-blue"></i>
-						<div class="d-inline">
-							<h5><?php echo $titulo;?></h5>
-							<span><?php echo $sub_titulo;?></span>
+	<?php $this->load->view('layout/sidebar') ?>
+	<div class="main-content">
+		<div class="container-fluid">
+			<div class="page-header">
+				<div class="row align-items-end">
+					<div class="col-lg-8">
+						<div class="page-header-title">
+							<i class="<?php echo $icone_view; ?> bg-blue"></i>
+							<div class="d-inline">
+								<h5><?php echo $titulo; ?></h5>
+								<span><?php echo $sub_titulo; ?></span>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4">
+						<nav class="breadcrumb-container" aria-label="breadcrumb">
+							<ol class="breadcrumb">
+								<li class="breadcrumb-item">
+									<a data-toggle="tooltip" data-placement="bottom" title="Home" href="<?php echo base_url(); ?>"><i class="ik ik-home"></i></a>
+								</li>
+								<li class="breadcrumb-item">
+									<a data-toggle="tooltip" data-placement="bottom" title="Lista <?php echo $this->router->fetch_class() ?>" href="<?php echo base_url($this->router->fetch_class()) ?>">Listar <?php echo $this->router->fetch_class() ?></a>
+								</li>
+								<li data-toggle="tooltip" data-placement="bottom" class="breadcrumb-item active" aria-current="page"><?php echo $titulo; ?></li>
+							</ol>
+						</nav>
+					</div>
+				</div>
+			</div>
+
+
+			<div class="row">
+				<div class="col-md-12">
+					<div class="card">
+						<div class="card-header"><?php echo (isset($usuario) ? '<i class="ik ik-calendar ik-2x"></i>Data da última alteração: ' . formata_data_banco_com_hora($usuario->data_ultima_alteracao) : '') ?></div>
+						<div class="card-body">
+							<form class="forms-sample" name="form_core" method="post">
+
+								<div class="form-group row">
+									<div class="col-md-6 mb-20">
+										<label>Nome</label>
+										<input type="text" class="form-control" name="first_name" value="<?php echo (isset($usuario) ? $usuario->first_name : set_value('first_name')) ?>">
+									</div>
+									<div class="col-md-6 mb-20">
+										<label>Sobrenome</label>
+										<input type="text" class="form-control" name="last_name" value="<?php echo (isset($usuario) ? $usuario->last_name : set_value('last_name')) ?>">
+									</div>
+								</div>
+
+								<div class="form-group row">
+									<div class="col-md-6 mb-20">
+										<label>Usuário</label>
+										<input type="text" class="form-control" name="username" value="<?php echo (isset($usuario) ? $usuario->username : set_value('username')) ?>">
+									</div>
+									<div class="col-md-6 mb-20">
+										<label>E-mail (Login)</label>
+										<input type="text" class="form-control" name="email" value="<?php echo (isset($usuario) ? $usuario->email : set_value('email')) ?>">
+									</div>
+								</div>
+
+								<div class="form-group row">
+									<div class="col-md-6 mb-20">
+										<label>Senha</label>
+										<input type="password" class="form-control" name="password" value="">
+									</div>
+									<div class="col-md-6 mb-20">
+										<label>Confirmação</label>
+										<input type="password" class="form-control" name="confirmacao">
+									</div>
+								</div>
+
+								<div class="form-group row">
+									<div class="col-md-6 mb-20">
+										<label>Perfil de acesso</label>
+										<select class="form-control" name="perfil">
+											<option value="2">Atendente</option>
+											<option value="1">Administrador</option>
+										</select>
+									</div>
+									<div class="col-md-6 mb-20">
+										<label>Ativo</label>
+										<select class="form-control" name="active">
+											<?php if (isset($usuario)) :  ?>
+												<option value="0" <?php echo ($usuario->active == 0 ? 'selected' : '') ?>>Não</option>
+												<option value="1" <?php echo ($usuario->active == 1 ? 'selected' : '') ?>>Sim</option>
+											<?php endif; ?>
+										</select>
+									</div>
+
+								</div>
+
+								<button type="submit" class="btn btn-primary mr-2">Submit</button>
+								<button class="btn btn-light">Cancel</button>
+							</form>
 						</div>
 					</div>
 				</div>
-				<div class="col-lg-4">
-					<nav class="breadcrumb-container" aria-label="breadcrumb">
-					<ol class="breadcrumb">
-							<li class="breadcrumb-item">
-								<a  data-toggle="tooltip" data-placement="bottom" title="Home" href="<?php echo base_url(); ?>"><i class="ik ik-home"></i></a>
-							</li>
-							<li class="breadcrumb-item">
-								<a  data-toggle="tooltip" data-placement="bottom" title="Lista <?php echo $this->router->fetch_class()?>" href="<?php echo base_url($this->router->fetch_class())?>">Listar <?php echo $this->router->fetch_class()?></a>
-							</li>
-							<li  data-toggle="tooltip" data-placement="bottom" class="breadcrumb-item active" aria-current="page"><?php echo $titulo;?></li>
-						</ol>
-					</nav>
-				</div>
 			</div>
+
 		</div>
+	</div>
 
 
-		<div class="row">
-			<div class="col-md-12">
-				<div class="card">
-					<div class="card-header"><?php echo (isset($usuario)? $usuario->data_ultima_alteracao: '') ?></div>
-					<div class="card-body">
-						<form class="forms-sample">
-							<div class="form-group">
-								<label for="exampleInputUsername1">Username</label>
-								<input type="text" class="form-control" id="exampleInputUsername1" placeholder="Username">
-							</div>
-							<div class="form-group">
-								<label for="exampleInputEmail1">Email address</label>
-								<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-							</div>
-							<div class="form-group">
-								<label for="exampleInputPassword1">Password</label>
-								<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-							</div>
-							<div class="form-group">
-								<label for="exampleInputConfirmPassword1">Confirm Password</label>
-								<input type="password" class="form-control" id="exampleInputConfirmPassword1" placeholder="Password">
-							</div>
-							<div class="form-group">
-								<label class="custom-control custom-radio">
-									<input type="radio" class="custom-control-input">
-									<span class="custom-control-label">&nbsp;Remember me</span>
-								</label>
-							</div>
-							<button type="submit" class="btn btn-primary mr-2">Submit</button>
-							<button class="btn btn-light">Cancel</button>
-						</form>
-					</div>
-				</div>
-			</div>
+	<footer class="footer">
+		<div class="w-100 clearfix">
+			<span class="text-center text-sm-left d-md-inline-block">Copyright © <?php echo date('Y') ?> ThemeKit v2.0. All Rights Reserved.</span>
+			<span class="float-none float-sm-right mt-1 mt-sm-0 text-center">Crafted with <i class="fa fa-code text-dark"></i> by <a href="javascript:void" class="text-dark" target="_blank">Francisco Miguel</a></span>
 		</div>
-
-	</div>
-</div>
-
-
-<footer class="footer">
-	<div class="w-100 clearfix">
-		<span class="text-center text-sm-left d-md-inline-block">Copyright © <?php echo date('Y')?> ThemeKit v2.0. All Rights Reserved.</span>
-		<span class="float-none float-sm-right mt-1 mt-sm-0 text-center">Crafted with <i class="fa fa-code text-dark"></i> by <a href="javascript:void" class="text-dark" target="_blank">Francisco Miguel</a></span>
-	</div>
-</footer>
+	</footer>
 
 </div>
-
-        
