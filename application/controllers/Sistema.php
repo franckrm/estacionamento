5c41e0a4f4509bda9857fcdbe6801e3426361af6
Ribeirovisk <franckrm2001@gmail.com>
2024-04-15 20:05:26 -0300

    feat: create view to update company data

--- application/controllers/Sistema.php
+++ application/controllers/Sistema.php
@@ -17,14 +17,18 @@ class Sistema extends CI_Controller{
 			'titulo' => 'Editar Informações do sistema',
 			'sub_titulo' => 'Chegou a hora de editar as informações do  sisterma',
 			'icone_view' => 'ik ik-settings',
+			'scripts'=>array(
+				'plugins/mask/jquery.mask.min.js',
+				'plugins/mask/custom.js'
+			),
 			'sistema' => $this->core_model->get_by_id('sistema', array('sistema_id'=> 1))
 
 		);
 
-		var_dump($data['sistema']); 
+		//var_dump($data['sistema']); die;
 
 		$this->load->view('layout/header', $data);
-		$this->load->view('usuarios/core');
+		$this->load->view('sistema/index');
 		$this->load->view('layout/footer');
 	}
 }
